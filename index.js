a6cb494c633e3bdcc692f2cd5150df7780ffebd6
Falgun Jairaj Thakwani <fl700637@dal.ca>
2023-03-16 11:27:11 -0300

    Added dist folder

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
-const router = require('../router/routes');
+const router = require('./router/routes');
 app.use(express.json());
 
 
 app.use('/', router);
+app
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
